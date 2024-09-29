9b063492dc6817c26164a2f1c8a7f3ba898bedc4
cameroncolley <103643997+ST10037966@users.noreply.github.com>
2024-09-29 17:11:24 +0200

    Adding audio Pages

---
+++ settings.gradle.kts
@@ -0,0 +1,27 @@
+pluginManagement {
+    repositories {
+        google {
+            content {
+                includeGroupByRegex("com\\.android.*")
+                includeGroupByRegex("com\\.google.*")
+                includeGroupByRegex("androidx.*")
+            }
+        }
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+        maven(url = "https://jitpack.io") // Use parentheses for the url parameter
+    }
+}
+
+rootProject.name = "OPSC7312poe"
+include(":app")
+
+ 
